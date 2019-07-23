6e31db4d11aa3db5e1bd9d001c68bb7f96470f86
Yong <freeyongx@163.com>
2019-07-23 12:24:08 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,13 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    /**
+     * 显示用户个人资料页面
+     *
+     * @param \App\Models\User $user
+     *
+     * @return void
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * 编辑个人资料页面
+     *
+     * @param \App\Models\User $user
+     *
+     * @return void
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 更新用户资料
+     *
+     * @param \App\Http\Requests\UserRequest $request
+     * @param \App\Models\User $user
+     *
+     * @return void
+     */
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }

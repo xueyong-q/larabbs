77ce7cbd3ef61e8d82f181a09a22b7631b441484
Yong <freeyongx@163.com>
2019-07-24 23:46:39 +0000

    上传头像

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Handlers;
+
+use Illuminate\Support\Str;
+
+class ImageUploadHandler
+{
+    protected $allowed_ext = ['png', 'jpg', 'git', 'jpeg'];
+
+    /**
+     * 保存上传文件
+     *
+     * @param [type] $file
+     * @param [type] $folder
+     * @param [type] $file_prefix
+     *
+     * @return void
+     */
+    public function save($file, $folder, $file_prefix)
+    {
+        $folder_name = "uploads/images/$folder/" . date('Ym/d', time());
+
+        $upload_path = public_path() . '/' . $folder_name;
+
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+
+        $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+
+        if (!in_array($extension, $this->allowed_ext)) {
+            return false;
+        }
+
+        $file->move($upload_path, $filename);
+
+        return [
+            'path' => config('app.url') . "/$folder_name/$filename"
+        ];
+    }
+}

91a2c833a217426cde8b4a3f87b8c46fa0f23dfe
Yong <freeyongx@163.com>
2019-08-11 12:09:56 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,56 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * 设置排序并处理 N+1 问题
+     *
+     * @param [type] $query
+     * @param [type] $order
+     *
+     * @return void
+     */
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+
+    /**
+     * 按更新时间倒序
+     *
+     * @param [type] $query
+     *
+     * @return void
+     */
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * 按写入时间倒序
+     *
+     * @param [type] $query
+     *
+     * @return void
+     */
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
 }

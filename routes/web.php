bc63817dfe1db6228e34c3300da14dccd17a3b49
Yong <freeyongx@163.com>
2019-09-05 08:26:27 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 图片上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');

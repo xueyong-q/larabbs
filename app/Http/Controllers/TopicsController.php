5de317cc2161b6a28131e2bacaf4826d16bdf4d5
Yong <freeyongx@163.com>
2020-04-05 09:27:56 +0000

    侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -23,11 +24,13 @@ class TopicsController extends Controller
 	 *
 	 * @return void
 	 */
-	public function index(Request $request, Topic $topic, User $user)
+	public function index(Request $request, Topic $topic, User $user, Link $link)
 	{
 		$topics = $topic->withOrder($request->order)->paginate(20);
 		$active_users = $user->getActiveUsers();
-		return view('topics.index', compact('topics', 'active_users'));
+		$links = $link->getAllCached();
+
+		return view('topics.index', compact('topics', 'active_users', 'links'));
 	}
 
 	/**

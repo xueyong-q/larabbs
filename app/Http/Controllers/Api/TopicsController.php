7eaf9f7dce55087544dafe88c34f228b090fe907
Yong <freeyongx@163.com>
2020-07-26 13:43:29 +0000

    话题修改

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -10,11 +10,35 @@ use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
+    /**
+     * 发布文章
+     *
+     * @param \App\Http\Requests\Api\TopicRequest $resuest
+     * @param \App\Models\Topic $topic
+     *
+     * @return void
+     */
     public function store(TopicRequest $resuest, Topic $topic)
     {
         $topic->fill($resuest->all());
         $topic->user_id = $resuest->user()->id;
         $topic->save();
+
+        return new TopicResource($topic);
+    }
+
+    /**
+     * 更新文章
+     *
+     * @param \App\Http\Requests\Api\TopicRequest $request
+     * @param \App\Models\Topic $topic
+     *
+     * @return void
+     */
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
         return new TopicResource($topic);
     }

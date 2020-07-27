96acec0b427404955ac89a2ffc072a1820310cf5
Yong <freeyongx@163.com>
2020-07-27 10:27:40 +0000

    话题详情

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Queries\TopicQuery;
 use App\Http\Requests\Api\TopicRequest;
 use App\Http\Resources\TopicResource;
 use App\Models\Topic;
@@ -21,24 +22,10 @@ class TopicsController extends Controller
      *
      * @return void
      */
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, TopicQuery $query)
     {
-        $query = $topic->query();
-
-        if ($categoryId = $request->category_id) {
-            $query->where('category_id', $categoryId);
-        }
-
-        $topics = QueryBuilder::for(Topic::class)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
-
-        // $topics = $query->withOrder($request->order)->paginate();
+        
+        $topics = $query->paginate();
 
         return TopicResource::collection($topics);
     }
@@ -51,18 +38,9 @@ class TopicsController extends Controller
      *
      * @return void
      */
-    public function userIndex(Request $request, User $user)
+    public function userIndex(Request $request, User $user, TopicQuery $query)
     {
-        $query = $user->topics()->getQuery();
-
-        $topics = QueryBuilder::for($query)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        $topics = $query->where('user_id', $user->id)->paginate();
 
         return TopicResource::collection($topics);
     }
@@ -112,6 +90,20 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
 
         $topic->delete();
+
+        return new TopicResource($topic);
+    }
+
+    /**
+     * 文章详情
+     *
+     * @param \App\Models\Topic $topic
+     *
+     * @return void
+     */
+    public function show($topicId, TopicQuery $query)
+    {
+        $topic = $query->findOrFail($topicId);
 
         return new TopicResource($topic);
     }

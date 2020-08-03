a4a41c9894a92506c64ea9c75710d3fa02ad1d30
Yong <freeyongx@163.com>
2020-08-03 08:42:20 +0000

    修复个人中心bug

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,9 +14,17 @@
             <p>{{ $user->introduction }}</p>
             <hr>
             <h5><strong>注册于</strong></h5>
-            <p>{{ $user->created_at->diffForHumans() }}</p>
+            <p>
+              @if(empty($user->created_at))
+                null
+              @else
+                {{ $user->created_at->diffForHumans() }}
+              @endif
+            </p>
             <h5><strong>最后活跃</strong></h5>
-            <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
+            <p title="{{  $user->last_actived_at }}">
+              {{ $user->last_actived_at->diffForHumans() }}
+            </p>
       </div>
     </div>
   </div>
